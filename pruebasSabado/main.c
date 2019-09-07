13e85085eb5c42b617673f47ab2206846bb54dc2
juliobana <47338088+juliobana@users.noreply.github.com>
2019-09-07 10:39:57 -0300

    Add files via upload

---
+++ pruebasSabado/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+
+}
+int DameElMaximo(int parametrosArray[indice];int cant)
+ int indice;
+ int maximo;
+// sacar el maximo
+ for(indice=0;indice<cant;indice++){
+    if(indice ==0 || parametrosArray[indice]>maximo){
+        maximo=parametrosArray[indice];
+    }
+ }
+return maximo;
+
+/**************************************************/
+int DameElIndiceMaximo(int parametrosArray[];int cant)
+ int indice;
+ int IndiceDelMaximo;
+for(indice=0;indice<cant;indice++){
+    if(indice ==0 || parametrosArray[indice]>maximo){
+        maximo=parametrosArray[indice];
+        IndiceDelMaximo=indice;
+    }
+ }
+return IndiceDelMaximo;
